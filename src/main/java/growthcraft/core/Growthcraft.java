b3b3ca67a0388a37940d2249d4e5c07a718c229d
alatyami <alatyami@gmail.com>
2025-09-19 15:32:48 -0600

    **Moved creative tab registration to a dedicated class for better maintainability.**

--- src/main/java/growthcraft/core/Growthcraft.java
+++ src/main/java/growthcraft/core/Growthcraft.java
@@ -3,12 +3,12 @@ package growthcraft.core;
 import growthcraft.core.config.GrowthcraftConfig;
 import growthcraft.core.config.Reference;
 import growthcraft.core.init.GrowthcraftBlocks;
+import growthcraft.core.init.GrowthcraftCreativeTabs;
 import growthcraft.core.init.GrowthcraftItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -16,7 +16,6 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -28,11 +27,11 @@ public class Growthcraft {
     public Growthcraft(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(this::buildCreativeTabs);
 
         // Register Deferred Registers
         GrowthcraftItems.ITEMS.register(modEventBus);
         GrowthcraftBlocks.BLOCKS.register(modEventBus);
+        GrowthcraftCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
 
         NeoForge.EVENT_BUS.register(this);
 
@@ -41,27 +40,6 @@ public class Growthcraft {
 
     private void commonSetup(FMLCommonSetupEvent event) {
         LOGGER.info("HELLO FROM COMMON SETUP");
-    }
-
-    private void buildCreativeTabs(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-            event.accept(GrowthcraftItems.CROWBAR_WHITE);
-            event.accept(GrowthcraftItems.CROWBAR_LIGHT_GRAY);
-            event.accept(GrowthcraftItems.CROWBAR_GRAY);
-            event.accept(GrowthcraftItems.CROWBAR_BLACK);
-            event.accept(GrowthcraftItems.CROWBAR_BROWN);
-            event.accept(GrowthcraftItems.CROWBAR_RED);
-            event.accept(GrowthcraftItems.CROWBAR_ORANGE);
-            event.accept(GrowthcraftItems.CROWBAR_YELLOW);
-            event.accept(GrowthcraftItems.CROWBAR_LIME);
-            event.accept(GrowthcraftItems.CROWBAR_GREEN);
-            event.accept(GrowthcraftItems.CROWBAR_CYAN);
-            event.accept(GrowthcraftItems.CROWBAR_LIGHT_BLUE);
-            event.accept(GrowthcraftItems.CROWBAR_BLUE);
-            event.accept(GrowthcraftItems.CROWBAR_PURPLE);
-            event.accept(GrowthcraftItems.CROWBAR_MAGENTA);
-            event.accept(GrowthcraftItems.CROWBAR_PINK);
-        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/growthcraft/core/init/GrowthcraftCreativeTabs.java
@@ -0,0 +1,39 @@
+package growthcraft.core.init;
+
+import growthcraft.core.config.Reference;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class GrowthcraftCreativeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Reference.MODID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN = CREATIVE_MODE_TABS.register(
+            Reference.UnlocalizedName.CreativeTab.TAB,
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(GrowthcraftItems.CROWBAR_WHITE.get()))
+                    .title(Component.literal("Growthcraft"))
+                    .displayItems((params, output) -> {
+                        output.accept(GrowthcraftItems.CROWBAR_WHITE.get());
+                        output.accept(GrowthcraftItems.CROWBAR_LIGHT_GRAY.get());
+                        output.accept(GrowthcraftItems.CROWBAR_GRAY.get());
+                        output.accept(GrowthcraftItems.CROWBAR_BLACK.get());
+                        output.accept(GrowthcraftItems.CROWBAR_BROWN.get());
+                        output.accept(GrowthcraftItems.CROWBAR_RED.get());
+                        output.accept(GrowthcraftItems.CROWBAR_ORANGE.get());
+                        output.accept(GrowthcraftItems.CROWBAR_YELLOW.get());
+                        output.accept(GrowthcraftItems.CROWBAR_LIME.get());
+                        output.accept(GrowthcraftItems.CROWBAR_GREEN.get());
+                        output.accept(GrowthcraftItems.CROWBAR_CYAN.get());
+                        output.accept(GrowthcraftItems.CROWBAR_LIGHT_BLUE.get());
+                        output.accept(GrowthcraftItems.CROWBAR_BLUE.get());
+                        output.accept(GrowthcraftItems.CROWBAR_PURPLE.get());
+                        output.accept(GrowthcraftItems.CROWBAR_MAGENTA.get());
+                        output.accept(GrowthcraftItems.CROWBAR_PINK.get());
+                    })
+                    .build()
+    );
+}
