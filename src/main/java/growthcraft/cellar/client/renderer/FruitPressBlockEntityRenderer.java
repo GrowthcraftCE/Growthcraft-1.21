df1bc8937c0fbff4aec0a3ed9e1f751b9db96908
alatyami <alatyami@gmail.com>
2026-07-05 11:40:07 -0600

    #103 raise fruit press fluid surface

--- src/main/java/growthcraft/cellar/client/renderer/FruitPressBlockEntityRenderer.java
+++ src/main/java/growthcraft/cellar/client/renderer/FruitPressBlockEntityRenderer.java
@@ -9,8 +9,8 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 
 public class FruitPressBlockEntityRenderer implements BlockEntityRenderer<FruitPressBlockEntity> {
     private static final MachineFluidRenderer.Bounds TRAY_BOUNDS = new MachineFluidRenderer.Bounds(
-            3.0F / 16.0F, 2.0F / 16.0F, 3.0F / 16.0F,
-            13.0F / 16.0F, 5.0F / 16.0F, 13.0F / 16.0F);
+            2.25F / 16.0F, 4.05F / 16.0F, 2.25F / 16.0F,
+            13.75F / 16.0F, 6.75F / 16.0F, 13.75F / 16.0F);
 
     public FruitPressBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
     }
