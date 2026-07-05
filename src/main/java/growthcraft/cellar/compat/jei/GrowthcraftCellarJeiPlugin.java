2809a7e4505bbec49f4bb5bc63af3fc4d5db69f2
alatyami <alatyami@gmail.com>
2026-07-04 18:55:09 -0600

    #65 Normalize Culture Jar recipes and JEI

--- src/main/java/growthcraft/cellar/compat/jei/GrowthcraftCellarJeiPlugin.java
+++ src/main/java/growthcraft/cellar/compat/jei/GrowthcraftCellarJeiPlugin.java
@@ -4,6 +4,7 @@ import growthcraft.cellar.config.Reference;
 import growthcraft.cellar.init.GrowthcraftCellarItems;
 import growthcraft.cellar.init.GrowthcraftCellarRecipes;
 import growthcraft.cellar.recipe.BrewKettleRecipe;
+import growthcraft.cellar.recipe.CultureJarRecipe;
 import growthcraft.cellar.recipe.FermentationBarrelRecipe;
 import growthcraft.cellar.recipe.FruitPressRecipe;
 import growthcraft.cellar.recipe.RoasterRecipe;
@@ -24,6 +25,8 @@ public class GrowthcraftCellarJeiPlugin implements IModPlugin {
     public static final ResourceLocation PLUGIN_UID = ResourceLocation.fromNamespaceAndPath(Reference.MODID, "jei_plugin");
     public static final RecipeType<RecipeHolder<BrewKettleRecipe>> BREW_KETTLE =
             new RecipeType<>(ResourceLocation.fromNamespaceAndPath(Reference.MODID, Reference.UnlocalizedName.Recipe.BREW_KETTLE_RECIPE), GrowthcraftCellarJeiPlugin.<BrewKettleRecipe>recipeHolderClass());
+    public static final RecipeType<RecipeHolder<CultureJarRecipe>> CULTURE_JAR =
+            new RecipeType<>(ResourceLocation.fromNamespaceAndPath(Reference.MODID, "culture_jar"), GrowthcraftCellarJeiPlugin.<CultureJarRecipe>recipeHolderClass());
     public static final RecipeType<RecipeHolder<FermentationBarrelRecipe>> FERMENTATION_BARREL =
             new RecipeType<>(ResourceLocation.fromNamespaceAndPath(Reference.MODID, Reference.UnlocalizedName.Recipe.FERMENT_BARREL_RECIPE), GrowthcraftCellarJeiPlugin.<FermentationBarrelRecipe>recipeHolderClass());
     public static final RecipeType<RecipeHolder<FruitPressRecipe>> FRUIT_PRESS =
@@ -46,6 +49,7 @@ public class GrowthcraftCellarJeiPlugin implements IModPlugin {
         var guiHelper = registration.getJeiHelpers().getGuiHelper();
         registration.addRecipeCategories(
                 new BrewKettleRecipeCategory(guiHelper),
+                new CultureJarRecipeCategory(guiHelper),
                 new FermentationBarrelRecipeCategory(guiHelper),
                 new FruitPressRecipeCategory(guiHelper),
                 new RoasterRecipeCategory(guiHelper)
@@ -58,6 +62,7 @@ public class GrowthcraftCellarJeiPlugin implements IModPlugin {
         if (minecraft.level == null) return;
 
         registration.addRecipes(BREW_KETTLE, minecraft.level.getRecipeManager().getAllRecipesFor(GrowthcraftCellarRecipes.BREW_KETTLE_TYPE.get()));
+        registration.addRecipes(CULTURE_JAR, minecraft.level.getRecipeManager().getAllRecipesFor(GrowthcraftCellarRecipes.CULTURE_JAR_TYPE.get()));
         registration.addRecipes(FERMENTATION_BARREL, minecraft.level.getRecipeManager().getAllRecipesFor(GrowthcraftCellarRecipes.FERMENTATION_BARREL_TYPE.get()));
         registration.addRecipes(FRUIT_PRESS, minecraft.level.getRecipeManager().getAllRecipesFor(GrowthcraftCellarRecipes.FRUIT_PRESS_TYPE.get()));
         registration.addRecipes(ROASTER, minecraft.level.getRecipeManager().getAllRecipesFor(GrowthcraftCellarRecipes.ROASTER_TYPE.get()));
@@ -66,6 +71,7 @@ public class GrowthcraftCellarJeiPlugin implements IModPlugin {
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
         registration.addRecipeCatalyst(new ItemStack(GrowthcraftCellarItems.BREW_KETTLE.get()), BREW_KETTLE);
+        registration.addRecipeCatalyst(new ItemStack(GrowthcraftCellarItems.CULTURE_JAR.get()), CULTURE_JAR);
         registration.addRecipeCatalyst(new ItemStack(GrowthcraftCellarItems.FERMENTATION_BARREL_OAK.get()), FERMENTATION_BARREL);
         registration.addRecipeCatalyst(new ItemStack(GrowthcraftCellarItems.FRUIT_PRESS.get()), FRUIT_PRESS);
         registration.addRecipeCatalyst(new ItemStack(GrowthcraftCellarItems.ROASTER.get()), ROASTER);
