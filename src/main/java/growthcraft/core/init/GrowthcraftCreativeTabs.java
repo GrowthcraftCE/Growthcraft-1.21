8383af0ca6a7728ec2d2bb9a9e48545a7f6ec638
alatyami <alatyami@gmail.com>
2026-07-06 17:57:41 -0600

    Remove legacy Apiary content for #115

--- src/main/java/growthcraft/core/init/GrowthcraftCreativeTabs.java
+++ src/main/java/growthcraft/core/init/GrowthcraftCreativeTabs.java
@@ -1,6 +1,7 @@
 package growthcraft.core.init;
 
 import growthcraft.core.config.Reference;
+import growthcraft.cellar.init.GrowthcraftCellarItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
@@ -14,7 +15,7 @@ public class GrowthcraftCreativeTabs {
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN = CREATIVE_MODE_TABS.register(
             Reference.UnlocalizedName.CreativeTab.TAB,
             () -> CreativeModeTab.builder()
-                    .icon(() -> new ItemStack(GrowthcraftItems.CROWBAR_WHITE.get()))
+                    .icon(() -> new ItemStack(GrowthcraftCellarItems.FERMENTATION_BARREL_OAK.get()))
                     .title(Component.literal("Growthcraft"))
                     .displayItems((params, output) -> {
                         // Materials
